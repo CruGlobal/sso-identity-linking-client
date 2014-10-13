ccce886484b047ef65e7112abc5f9a1830257eb2
Lee Braddock <lee.braddock@cru.org>
2014-10-13 13:49:49 -0400

    Add LinkingException class.

---
+++ src/main/java/org/cru/silc/exception/LinkingException.java
@@ -0,0 +1,19 @@
+package org.cru.silc.exception;
+
+/**
+ * Created by lee.braddock on 10/13/14.
+ */
+public class LinkingException extends Exception
+{
+	private static final long serialVersionUID = -4324411778119490111L;
+
+	public LinkingException(Throwable cause)
+	{
+		super(cause);
+	}
+
+	public LinkingException(String message)
+	{
+		super(message);
+	}
+}
