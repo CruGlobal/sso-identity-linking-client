c18b194ee80ea0f7ae21a9bf038aec4527cb7b4f
Lee Braddock <lee.braddock@cru.org>
2014-10-13 14:25:17 -0400

    Reformatting

--- src/main/java/org/cru/silc/service/LinkingService.java
+++ src/main/java/org/cru/silc/service/LinkingService.java
@@ -3,16 +3,17 @@ package org.cru.silc.service;
 /**
  * Created by lee.braddock on 10/13/14.
  */
+
 import org.cru.silc.exception.LinkingException;
 import org.cru.silc.domain.Identity;
 
 public interface LinkingService
 {
-	public Identity getLinkedIdentityByProviderType(Identity identity, Identity.ProviderType providerType) throws
-			LinkingException;
+    public Identity getLinkedIdentityByProviderType(Identity identity, Identity.ProviderType providerType) throws
+            LinkingException;
 
-	public void linkIdentities(Identity first, Identity second) throws LinkingException;
+    public void linkIdentities(Identity first, Identity second) throws LinkingException;
 
-	public void deleteLink(Identity identity) throws LinkingException;
+    public void deleteLink(Identity identity) throws LinkingException;
 
 }
--- src/main/java/org/cru/silc/util/WebResourceBuilder.java
+++ src/main/java/org/cru/silc/util/WebResourceBuilder.java
@@ -3,6 +3,7 @@ package org.cru.silc.util;
 /**
  * Created by lee.braddock on 10/13/14.
  */
+
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
 
@@ -11,28 +12,30 @@ import java.util.Map;
 
 public class WebResourceBuilder
 {
-	public static WebResource.Builder build(String resource, Map<String, String> headers, MediaType acceptType, MediaType contentType)
-	{
-		Client client = Client.create();
+    public static WebResource.Builder build(String resource, Map<String, String> headers, MediaType acceptType,
+                                            MediaType contentType)
+    {
+        Client client = Client.create();
 
-		// construct a web resource
-		WebResource webResource = client.resource(resource);
+        // construct a web resource
+        WebResource webResource = client.resource(resource);
 
-		// get the web resource builder
-		WebResource.Builder builder	= webResource.getRequestBuilder();
+        // get the web resource builder
+        WebResource.Builder builder = webResource.getRequestBuilder();
 
-		// add headers
-		for (Map.Entry<String, String> entry : headers.entrySet())
-			builder.header(entry.getKey(), entry.getValue());
+        // add headers
+        for (Map.Entry<String, String> entry : headers.entrySet())
+            builder.header(entry.getKey(), entry.getValue());
 
-		// specify acceptable media type(s)
-		if(acceptType != null)
-			builder.accept(acceptType);
+        // specify acceptable media type(s)
+        if (acceptType != null)
+            builder.accept(acceptType);
 
-		// specify content media type(s)
-		if(contentType != null)
-			builder.type(contentType);
+        // specify content media type(s)
+        if (contentType != null)
+            builder.type(contentType);
 
-		return builder;
-	}
+        return builder;
+    }
 }
+
